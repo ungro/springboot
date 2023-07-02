cf5347f28371fbd38322d87c89832dbc57ad7ccd
ungro <chibou.420@gmail.com>
2023-07-02 11:22:05 +0900

    開発環境構築

---
+++ its/src/main/kotlin/com/example/its/ItsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.its
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ItsApplication>(*args)
+}
